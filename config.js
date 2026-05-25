f5371117bb5cce027adca686fa8671e813d08a3f
gleoleder <gleo90000@gmail.com>
2026-05-25 00:21:08 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// ========================================
+// CONFIGURACIÓN - DISFRACES FANTASÍA
+// ========================================
+
+const CONFIG = {
+    // URL del Google Apps Script (pegar aquí después de desplegar):
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxacS2VYaPuXR7VyeaeMF6R97CzvAHQZcBdLk69r8fq2-da7avKnNQX96SY6c96Fp-A/exec',
+    
+    // Sheet ID (solo referencia):
+    GOOGLE_SHEET_ID: '1cHNX8O2BvQRkhr_P5cww5I8hwvQoknBQabLQnjjICLE'
+};
